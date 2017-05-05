b4ea3e733d074972ac7524d76f5b117931a93576
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-05 17:57:26 +0200

    console log

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+
+
+
+
+/*Stwórz zmienne potrzebne do wykonania działania (a * a) + (2 * a * b) - (b * b) , a jego wynik przypisz do zmiennej value. 
+
+Wyświetl wynik w konsoli, a następnie za pomocą instrukcji warunkowej sprawdź, czy wynik jest dodatni czy ujemny,
+a potem wyświetl informację wynik dodatni lub wynik ujemny w konsoli.
+
+Dodaj instrukcję, która sprawdzi, czy wynik jest równy zero i wyświetli tę informację. */
