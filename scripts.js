18e66ad215f475e76cd6742ea335490945db4788
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-09 17:05:48 +0200

    DRY correction

--- scripts.js
+++ scripts.js
@@ -15,20 +15,19 @@ var verifyNewName = allNames.indexOf(newName);
 console.log("List of all names together:", allNames);
 console.log("A new name we would like to add:", newName);
 console.log("Verifying if a new name already exists in current table, possible results: \n-1 - a name does not exist \nother number - a name already exists, and this number indicates its' location in table \n \nResult for name", newName, ":", verifyNewName);
-s
+
 // adding new name to final table
 
 function addingNewName() {
     if (verifyNewName === -1) {
         var x = allNames.push(newName);
         console.log("Name", newName, "does not exists and will be added to final list of names.");
-        return console.log("Final list of names:", allNames);
-    } 
+    } else {
         console.log("Name", newName, "allready exists and will not be added to final list of names.");
-        return console.log("Final list of names:", allNames);
+    }
+    return console.log("Final list of names:", allNames);
 }
 
 addingNewName();
-
 
 
