3a48e5e0f3eacaeb9bc03ce82d86cb33ac1017ae
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-11 15:37:04 +0200

    2 poprawki - dowolny pierwszy wyraz oraz dzielenie dowolnego tekstu na pol

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
 var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
 console.log(text);
 
-var animal = "Zielone słonie";
+var animal = "zielone słonie";
 console.log(animal);
 
 var animalUpperCased = animal.toUpperCase();
 console.log(animalUpperCased);
 
-var textReplace = text.replace("Papugi", animalUpperCased);
+
+// Zrób tak żeby zamiast Papug, zamieniany był zawsze pierwszy wyraz zmiennej text - bez względu na to co tam będzie. Możesz użyć do tego metody split (google -> "mdn split")
+
+var firstWord = text.split(" ");
+var textReplace = text.replace(firstWord[0], animalUpperCased);
 console.log(textReplace);
+
+
+// Twój program zadziała tylko dla tego konkretnego zdania. Jeśli text ulegnie zmienie, to już nie wyświetlisz idealnie połowy tekstu. Spróbuj zrobić to tak żeby tekst dzielić na pół bez względu na to jak długi on będzie.
+
+var halfLength = textReplace.length/2;
 
 console.log("dlugosc polowy zmiennej textReplace:", textReplace.length/2);
 
-var partOfTextReplace = textReplace.slice(0, 45);
+var partOfTextReplace = textReplace.slice(0, halfLength);
 console.log(partOfTextReplace);
+
+
+
 
