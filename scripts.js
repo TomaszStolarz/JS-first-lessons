e80c04addec7974f9e7e7bab9047fad94e381eca
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-11 18:46:28 +0200

    8.9/pętle

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function drawtree(h) {
+
+    if (h <=0) {
+        console.log('Nieprawidłowa wartość - parament h powinien być dodatni');
+    } else {
+        for (var i = 1; i <= h; i++) {
+            var star = "";
+
+            for (var j = 1; j <= i; j++) {
+                var star = (star += "*");
+            }
+            console.log(star);
+        }
+    }
+ }
+
+drawtree(10);
+
