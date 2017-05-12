8c9312d17fc03018b729f735a3bf11a4287dc168
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-12 11:28:42 +0200

    final version

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+function displayButtonText(n) {
 
+    var buttonId = document.getElementById('button');
+    var buttonClass = buttonId.getElementsByClassName('button');
+    console.log(buttonClass);   
 
+    var buttonCount = buttonClass.length;
+    console.log('Liczba przyciskow z id button i klasa button:', buttonCount);
 
+    var n = buttonCount;
+    
+    for ( k = 0; k = n; k++) {
+        return alert(button.innerText);
+    }
+}
 
+displayButtonText();
 
