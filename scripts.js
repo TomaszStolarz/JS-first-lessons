27b0b1449bde7e8eb8232f50719cfefeb05a45fa
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-12 19:20:42 +0200

    initial commit + start of the project

---
+++ scripts.js
@@ -0,0 +1,24 @@
+var buttonElem = document.getElementById('addElem');
+
+buttonElem.addEventListener('click', )
+    checkOnclickEvent('button został kliknięty')
+
+
+
+
+
+
+
+// function checkOnclickEvent(param) {
+//      console.log(param); 
+//     } 
+// var inputElem = document.getElementById('js-inputValue'), 
+//     imageElem = document.getElementById('js-image'); 
+    
+// imageElem.addEventListener('click', function(e) { 
+//     checkOnclickEvent('image został kliknięty') 
+// }); 
+
+// inputElem.addEventListener('keypress', function(e) { 
+//     e.target.value += ' test '; 
+// });
