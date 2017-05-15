b73bb356128262eb4b504b727b770160fbdb92d9
Tomasz Stolarz <stolarz.tomasz@o2.pl>
2017-05-15 12:03:21 +0200

    final version

--- scripts.js
+++ scripts.js
@@ -1,43 +1,7 @@
-// function checkOnClickEvent (n) {
-//     console.log(n);
-// }
+var list = document.getElementById("list");
+var addButton = document.getElementById('js-addElem');
 
-var buttonElem = document.getElementById('js-addElem');
-var list = document.getElementById('list');
-
-buttonElem.addEventListener('click', function() {
-
-    var element = document.createElement('li');
-
-    element.innerHTML = 'item' + itemCount;
-    var li = document.getElementsByTagName('li'); 
-    var liCount = list.length;
-    console.log(liCount);
-    
-    var keywords = ['item']
-    var keywordsArray = keywords.split(', ');
-    var itemCount = keywords.replace ('0', liCount);
-
-    list.appendChild(element);
+addButton.addEventListener('click', function() {
+    var liItems = document.getElementsByTagName('li');  
+    list.innerHTML += "<li>item " + liItems.length + "</li>";
 });
-
-
-
-// var li = document.getElementsByTagName('li'); 
-// console.log(itemsByTagName);
-
-
-
-// function checkOnclickEvent(param) {
-//      console.log(param); 
-//     } 
-// var inputElem = document.getElementById('js-inputValue'), 
-//     imageElem = document.getElementById('js-image'); 
-    
-// imageElem.addEventListener('click', function(e) { 
-//     checkOnclickEvent('image został kliknięty') 
-// }); 
-
-// inputElem.addEventListener('keypress', function(e) { 
-//     e.target.value += ' test '; 
-// });
